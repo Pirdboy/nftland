28fd400f81800bef3be2dd82fa5ef2407fd1b1a4
shy <151686055@qq.com>
2022-10-30 10:56:46 +0800

    redeploy contract

--- react-app/src/constants/Addresses.js
+++ react-app/src/constants/Addresses.js
@@ -1,8 +1,8 @@
 // Goerli
 const BirdNFTAddress = '0x8978715a11D4c40dEFD7D2589Bc4e2078654D039';
 
-const NFTLandCollectionContractAddress = "0x94e462Bf36335c192919b1f10742A6B8EAd20F12";
-const NFTLandMarketContractAddress = "0xe4dcd09593a95F980638F1d2c3842E59E953d9bc";
+const NFTLandCollectionContractAddress = "0xAD48B195fb85796f8336AD370CE79e4d45655D83";
+const NFTLandMarketContractAddress = "0x144b57935B0c0D785FCEd2C935103af87309A0fA";
 
 export {
     NFTLandCollectionContractAddress,
