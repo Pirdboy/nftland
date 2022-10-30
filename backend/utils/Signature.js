32f4278578983a43232774747633108c6af1ef5b
shy <151686055@qq.com>
2022-10-30 09:45:29 +0800

    address case-insensitive

--- backend/utils/Signature.js
+++ backend/utils/Signature.js
@@ -1,22 +1,20 @@
-const {ethers} = require('ethers');
+const { ethers } = require('ethers');
 
 class Signature {
     static VerifyEIP191Signature(message, signature, signerAddress) {
-        if(!message || !signature || !signerAddress) {
+        if (!message || !signature || !signerAddress) {
             return false;
         }
         let address = ethers.utils.verifyMessage(message, signature);
-        console.log('VerifyEIP191Signature address', address);
-        return address === signerAddress;
+        return address.toLowerCase() === signerAddress.toLowerCase();
     }
 
     static VerifyEIP712Signature(domain, types, values, signature, signerAddress) {
-        if(!domain || !types || !values || !signature || !signerAddress) {
+        if (!domain || !types || !values || !signature || !signerAddress) {
             return false;
         }
         let address = ethers.utils.verifyTypedData(domain, types, values, signature);
-        console.log('VerifyEIP712Signature address',address);
-        return address === signerAddress;
+        return address.toLowerCase() === signerAddress.toLowerCase();
     }
 }
 
